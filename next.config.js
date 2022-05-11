32c12385b9587154a6931a1013f2dbe3707cb9d9
Ivan Garcia <ivangm180@gmail.com>
2022-05-10 19:38:41 -0500

    [ADD] Next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
+// This is the configuration of next.js
+// Environment variables: makes safer call to the API
+// redirects() Works to redirect to other page e.g: the 404 not found
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    API_URL_KEY: "url",
+  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/about",
+  //       destination: "/",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
