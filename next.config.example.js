73d9fe93597959c0a75309d3b11660bb9d2ab2b2
Ivan G <ivangm180@gmail.com>
2022-06-14 10:59:46 -0500

    [ADD] I forgot the next.config.example.js lol

---
+++ next.config.example.js
@@ -0,0 +1,26 @@
+// This is the configuration of next.js
+// Environment variables: makes safer call to the API
+// redirects() Works to redirect to other page e.g: the 404 not found
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    API_URL_KEY: "url",
+  },
+  images: {
+    // This domain is an example, delete it when the real has implemented.
+    domains: ["www.business2community.com"],
+  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/about",
+  //       destination: "/",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
+};
+
+module.exports = nextConfig;
