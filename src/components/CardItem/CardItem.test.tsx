db03245d5d182821f70fae2d4207fcda5a1ed0d0
lilianarestrepotorres <lilianarestrepot@gmail.com>
2022-06-15 14:48:22 -0500

    Testing ListItems component

--- src/components/CardItem/CardItem.test.tsx
+++ src/components/CardItem/CardItem.test.tsx
@@ -2,11 +2,7 @@ import CardItem from ".";
 import renderer from "react-test-renderer";
 
 it("should render card items correctly", () => {
-    const tree = renderer
-      .create(
-        <CardItem />
-      )
-      .toJSON();
-    console.log(tree);
-    expect(tree).toMatchSnapshot();
-  });
+  const tree = renderer.create(<CardItem />).toJSON();
+  console.log(tree);
+  expect(tree).toMatchSnapshot();
+});
--- src/hooks/useGetModules.js
+++ src/hooks/useGetModules.js
@@ -15,7 +15,7 @@ const useGetModules = (key) =>{
             //setItems(response)
             return response
         });
-    }, []);
+    }, [key]);
 };
 
 export {useGetModules};
