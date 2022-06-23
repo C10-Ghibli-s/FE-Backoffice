ff985ebc123b6a10fc502d79def5e303fc3832fc
lilianarestrepotorres <lilianarestrepot@gmail.com>
2022-06-23 12:34:51 -0500

    Adding search icon into input field

--- src/components/SearchFilter/index.jsx
+++ src/components/SearchFilter/index.jsx
@@ -5,17 +5,18 @@ export default function SearchFilter() {
       <div className="top-16 w-96">
         <div className="relative mt-1 rounded-md shadow-sm">
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <span className="text-gray-500 sm:text-sm"></span>
+            <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+
           </div>
           <input
             type="text"
             name="search"
             id="search"
-            className="block w-full py-2 pl-3 pr-12 border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            className="block w-full py-2 pl-10 pr-12 border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             placeholder="Search with name"
           />
           <div className="absolute inset-y-0 right-0 flex">
-              <button className="w-20 text-white cursor-pointer  rounded-r-md bg-sky-500 hover:bg-sky-600">
+              <button className="w-20 text-white cursor-pointer rounded-r-md bg-sky-500 hover:bg-sky-600">
                   <span>Search</span>
               </button>
           </div>
