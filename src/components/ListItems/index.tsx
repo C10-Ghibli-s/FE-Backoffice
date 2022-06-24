f8296bda766108086e6a4dd8bf04c7720124bdd3
lilianarestrepotorres <lilianarestrepot@gmail.com>
2022-06-24 15:02:32 -0500

    Moving the place where is called the hook state

--- src/components/ListItems/index.tsx
+++ src/components/ListItems/index.tsx
@@ -1,47 +1,17 @@
 import React, { useEffect, useState } from "react";
 import CardItem from "@components/CardItem";
-import {useGetModules} from '../../hooks/useGetModules'
-
-// It should be replaced when we have db connection
-const itemsData = [
-  {
-    name: "Item 1",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-  },
-  {
-    name: "Item 2",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-  },
-  {
-    name: "Item 3",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-  },
-  {
-    name: "Item 4",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-  },
-  {
-    name: "Item 5",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
-  },
-];
 
 
-const ListItems = () => {
+function ListItems(data:any) {
   // it should use query selector
-  const {items}:any = useGetModules('public2')
-  console.log("items", items)
+  //const {items}:any = useGetModules('public2')
+  console.log("items from ListItems component...", data.data.items)
 
   return (
     <>
       <section className="relative flex flex-col items-center justify-center px-4 mx-8 my-4 bg-white sm:flex rounded-xl max-w-7xl sm:px-6 lg:px-8">
         <div className="mt-10 mb-10 md:grid md:grid-cols-2 md:gap-x-0 md:gap-y-0 md:after:content-[''] md:after:bg-[#e5e7eb] md:after:absolute md:after:h-[calc(100%-5rem)] md:after:right-1/2 md:after:w-[2px]" >
-          {items.map((item: { id: string; }) => (
+          {data.data.items.map((item: { id: string; }) => (
             <CardItem item={item} key={item.id}/>
           ))}
         </div>
--- src/pages/showmodules/index.jsx
+++ src/pages/showmodules/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import Title from "../../components/Title";
@@ -8,11 +8,16 @@ import StatusFilter from "../../components/StatusFilter";
 import ListItems from "../../components/ListItems";
 import { Header } from "../../components/Header"
 
+import {useGetModules} from '../../hooks/useGetModules';
+
 function ShowModules() {
   //Getting Module Title from Query Selector
   const router = useRouter();
   const query = router.query;
   const titleModule = query.nameModule;
+
+  const {items} = useGetModules('public2');
+  console.log("items from pages", items);
 
   return (
   <>
@@ -23,12 +28,12 @@ function ShowModules() {
     <Header/>
     <Title title={titleModule ? titleModule : "Show Module"}/>
     <section className="flex items-end justify-center min-h-full gap-6 px-4 py-16 sm:px-6 lg:px-8">
-      <SearchFilter />
+      <SearchFilter data={{items}} />
       <SortFilter />
       <StatusFilter />
     </section>
     <section className="flex flex-col items-center justify-center my-8">
-      <ListItems />
+      <ListItems data={{items}}/>
     </section>
     
     
