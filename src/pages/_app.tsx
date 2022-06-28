382dc035c1ef933ae68fecb32d1ef690542cccb3
Ivan G <ivangm180@gmail.com>
2022-06-28 10:47:39 -0500

    [ADD] Implementing login authentication

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,21 @@
-import '@styles/globals.css';
-import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
-import React, { useEffect, useRef } from 'react';
+import "@styles/globals.css";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import React, { useEffect, useRef } from "react";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  let sessionStatus = useRef<string|null>()
+  let sessionStatus = useRef<string | null>();
   useEffect(() => {
     sessionStatus.current = window.localStorage.getItem("localActiveSession");
-    if(sessionStatus.current == "no active session") {
+    if (sessionStatus.current == "no active session") {
       //Push the user to login page if isn't logged yet
-      router.push('/login');
-    };
-      // eslint-disable-next-line react-hooks/exhaustive-deps
+      router.push("/login");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  
+
   // ERROR HANDLING
   // axios.post("http://localhost:3000/graphql", {query: `mutation UpdateDirector($updateDirectorId: ID!, $data: DirecotrEditInput!) {
   //   updateDirector(id: $updateDirectorId, data: $data) {
@@ -25,7 +26,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   //   "updateDirectorId": "2",
   //   "data": {
   //   "name": "zqwe"
-  //   } 
+  //   }
   // }},
   //  {
   //   headers: {
@@ -33,12 +34,14 @@ function MyApp({ Component, pageProps }: AppProps) {
   //   }
   // })
   // .then(res => {
-  //   typeof JSON.parse(res.request.response).errors !== "undefined" ? console.error(JSON.parse(res.request.response).errors[0].message) : console.log('no hay errores xd') 
+  //   typeof JSON.parse(res.request.response).errors !== "undefined" ? console.error(JSON.parse(res.request.response).errors[0].message) : console.log('no hay errores xd')
   // })
   // .catch(err => console.log(err))
   return (
+    <UserProvider>
       <Component {...pageProps} />
-  ); 
-};
+    </UserProvider>
+  );
+}
 
-export default MyApp
+export default MyApp;
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,24 +1,33 @@
 import type { NextPage } from "next";
-import React from 'react';
-import LoginForm from '@components/LoginForm';
+import React from "react";
+import LoginForm from "@components/LoginForm";
+import { useUser } from "@auth0/nextjs-auth0";
 
-import LoginFacebookButton from '@components/LoginFacebookButton';
-import LoginTwitterButton from '@components/LoginTwitterButton';
+import LoginFacebookButton from "@components/LoginFacebookButton";
+import LoginTwitterButton from "@components/LoginTwitterButton";
 import Portal from "src/HOC/modal";
 
 const Login: NextPage = () => {
+  const { user, error, isLoading } = useUser();
+  console.log(user)
+
   return (
-      <main className="grid items-center justify-center w-screen h-screen grid-rows-2 bg-gray-100 md:flex">
-        <div className="flex flex-col items-center justify-center w-full row-start-2 row-end-3 mt-10 md:mt-0 md:w-2/5 h-3/4">
-          <LoginTwitterButton/>
-          <LoginFacebookButton/>
-        </div>
-        <div className="flex flex-col w-full row-start-1 row-end-2 mt-10 font-bold md:w-2/5 h-3/4 justify-evenly md:mt-0 md:p-10 md:border-l border-l-gray-300">
-          <h1 className="text-2xl text-center">Studio Ghibli <br/><span className="text-sky-600">BackOffice</span></h1>
-          <LoginForm/>
-        </div>
-      </main>
+    <main className="grid items-center justify-center w-screen h-screen grid-rows-2 bg-gray-100 md:flex">
+      <a href="/api/auth/login">Login</a>
+      <a href="/api/auth/logout">Logout</a>
+      <div className="flex flex-col items-center justify-center w-full row-start-2 row-end-3 mt-10 md:mt-0 md:w-2/5 h-3/4">
+        <LoginTwitterButton />
+        <LoginFacebookButton />
+      </div>
+      <div className="flex flex-col w-full row-start-1 row-end-2 mt-10 font-bold md:w-2/5 h-3/4 justify-evenly md:mt-0 md:p-10 md:border-l border-l-gray-300">
+        <h1 className="text-2xl text-center">
+          Studio Ghibli <br />
+          <span className="text-sky-600">BackOffice</span>
+        </h1>
+        <LoginForm />
+      </div>
+    </main>
   );
-}
+};
 
 export default Login;
