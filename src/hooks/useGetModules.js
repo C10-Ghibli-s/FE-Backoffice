9ee73656533fa211477645213897e1188c53fbb2
lilianarestrepotorres <lilianarestrepot@gmail.com>
2022-07-11 15:22:49 -0500

    Fixing filter functionality

--- src/hooks/useGetModules.js
+++ src/hooks/useGetModules.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import { GET_ITEM } from '@services/queries/getData/getItem';
-import axios from 'axios';
+import { GET_ITEM } from "@services/queries/getData/getItem";
+import axios from "axios";
 
 // useGetModules = (key, {setItems})
 const useGetModules = (titleModule) => {
@@ -9,84 +9,102 @@ const useGetModules = (titleModule) => {
   // users: getAllUsers, movies: getAllMovies, writers: getAllWriters, directors: getAllDirectors, musicians, getAllMusicians, roles: getAllRoles
   const [items, setItems] = useState([]);
   useEffect(() => {
-      setTimeout(() => {
-        axios.post(
-          process.env.API_URL !== undefined ? process.env.API_URL : '',
+    setTimeout(() => {
+      axios
+        .post(
+          process.env.API_URL !== undefined ? process.env.API_URL : "",
           // @ts-ignore
           GET_ITEM(titleModule),
           {
             headers: {
               "Content-Type": "application/json",
-            }
+            },
           }
         )
         .then((response) => {
-          if(response.data.data?.showAllUsers) setItems(response.data.data.showAllUsers)
-          if(response.data.data?.showAllMovies) setItems(response.data.data.showAllMovies)
-          if(response.data.data?.showAllWriters) setItems(response.data.data.showAllWriters)
-          if(response.data.data?.showAllDirectors) setItems(response.data.data.showAllDirectors)
-          if(response.data.data?.showAllMusicians) setItems(response.data.data.showAllMusicians)
-          console.log('response: ', response.data.data.showAllUsers);
-        }
-        )
+          if (response.data.data?.showAllUsers)
+            setItems(response.data.data.showAllUsers);
+          if (response.data.data?.showAllMovies)
+            setItems(response.data.data.showAllMovies);
+          if (response.data.data?.showAllWriters)
+            setItems(response.data.data.showAllWriters);
+          if (response.data.data?.showAllDirectors)
+            setItems(response.data.data.showAllDirectors);
+          if (response.data.data?.showAllMusicians)
+            setItems(response.data.data.showAllMusicians);
+          console.log("response: ", response.data.data.showAllUsers);
+        })
         .catch((error) => {
           console.log(error);
-        })
-        console.log('ModuleData: ', items, "QUERY : ", titleModule);
-      }, 1000);
-  
+        });
+      console.log("ModuleData: ", items, "QUERY : ", titleModule);
+    }, 1000);
   }, []);
 
-  const searchValue = (textTyped) =>{
+  const searchValue = (textTyped) => {
     const newArray = [...items];
-    const resultArray = 
-    textTyped === ""
-      ? items
-      : newArray.filter((item) =>
-      item?.title 
-        .toLowerCase()
-        .replace(/\s+/g, "")
-        .includes(textTyped.toLowerCase().replace(/\s+/g, "")));
+    const resultArray =
+      textTyped === ""
+        ? items
+        : newArray.filter((item) =>
+            item?.title
+              .toLowerCase()
+              .replace(/\s+/g, "")
+              .includes(textTyped.toLowerCase().replace(/\s+/g, ""))
+          );
     setItems(resultArray);
   };
 
-  const orderItems = (order, titleModule)=>{
+  const orderItems = (order, titleModule) => {
     const sortPropertyNumber = "id";
     let sortPropertyString = "name";
-    console.log({titleModule});
-    if(titleModule === "Movies"){
+    console.log({ titleModule });
+    if (titleModule === "Movies") {
       sortPropertyString = "title";
-    } else if(titleModule === "Users"){
+    } else if (titleModule === "Users") {
       sortPropertyString = "nickname";
-    } else{
+    } else {
       console.log("No sort property");
     }
     const newArray = [...items];
     const sortedNumber =
-    order === "Ascendent"
-      ? newArray.sort((firstItem, secondItem) => firstItem[sortPropertyNumber] - secondItem[sortPropertyNumber])
-      : newArray.sort((firstItem, secondItem) => secondItem[sortPropertyNumber] - firstItem[sortPropertyNumber]);
-    
+      order === "Ascendent"
+        ? newArray.sort(
+            (firstItem, secondItem) =>
+              firstItem[sortPropertyNumber] - secondItem[sortPropertyNumber]
+          )
+        : newArray.sort(
+            (firstItem, secondItem) =>
+              secondItem[sortPropertyNumber] - firstItem[sortPropertyNumber]
+          );
+
     const sortedString =
-    order.name === "Ascendent"
-      ? newArray.sort((firstItem, secondItem) => firstItem[sortPropertyString].localeCompare(secondItem[sortPropertyString]))
-      : newArray.sort((firstItem, secondItem) => secondItem[sortPropertyString].localeCompare(firstItem[sortPropertyString]));
-  
+      order.name === "Ascendent"
+        ? newArray.sort((firstItem, secondItem) =>
+            firstItem[sortPropertyString].localeCompare(
+              secondItem[sortPropertyString]
+            )
+          )
+        : newArray.sort((firstItem, secondItem) =>
+            secondItem[sortPropertyString].localeCompare(
+              firstItem[sortPropertyString]
+            )
+          );
+
     setItems(sortedString);
   };
 
-  const filterStatus = (filter, items)=>{
-    console.log("items from filterStatus",items);
+  const filterStatus = (filter, items) => {
     const newArray = [...items];
-    if (filter.name == 'All') {
+    if (filter.name == "ALL") {
       setItems(newArray);
-    } else{
-      const resultArray = newArray.filter(item => item.status == filter.name);
+    } else {
+      const resultArray = newArray.filter((item) => item.status == filter.name);
       setItems(resultArray);
     }
   };
 
-  return{items, setItems, searchValue, orderItems, filterStatus};
-}
+  return { items, setItems, searchValue, orderItems, filterStatus };
+};
 
 export { useGetModules };
